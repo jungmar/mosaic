9d465654abd341ec75b274f0796aa431d4841592
jungmar <149608756+jungmar@users.noreply.github.com>
2026-05-15 19:45:44 +0200

    Initial version

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'wildnotes-matrix-v1';
+const ASSETS = [
+  './',
+  './matrix.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './icons/icon-192.svg',
+  './icons/icon-512.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then((networkResponse) => {
+        const requestUrl = new URL(event.request.url);
+        if (requestUrl.origin === self.location.origin) {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return networkResponse;
+      });
+    })
+  );
+});
